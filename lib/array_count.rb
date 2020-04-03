b99078bd840c3d8cca764eeb827f3557bdb1d095
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-03 00:22:14 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -3,7 +3,7 @@ def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
   array.count do |i|
     new_array = []
-    if !i.is_a?(String)
+    if !i == "" || !i == " "
      array.pop
    end
 end
