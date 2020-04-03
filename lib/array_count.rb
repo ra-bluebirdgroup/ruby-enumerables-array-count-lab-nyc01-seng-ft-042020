6d1790f27e0d10dc2c5162cbd9e18b42bf1b5372
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-04-03 00:15:22 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -2,7 +2,7 @@ def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
   array.count do |i|
     new_array = []
-    if i.is_a?(String)
+    if i == "" || " "
      new_array << i
    end
 new_array.size
@@ -12,11 +12,11 @@ end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
-  array.count do |i|
-
-    if i == "" || " "
-    array.pop
-   end
-  array.length
-end
+#   array.count do |i|
+#
+#     if i == "" || " "
+#     array.pop
+#    end
+#   array.length
+# end
 end
